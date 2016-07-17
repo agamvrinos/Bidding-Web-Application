466b71e03e1e39d6ac0a73ee8490a6d1023fe600
p1ranhas <sdi1100006@di.uoa.gr>
2016-07-17 09:37:35 +0300

    Duplicate Username Check. Front End sux

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.*;
 
 public class UserDAO {
 
+    private static final Integer USERNAME_EXISTS_ERROR = -2;
     private static final String SQL_ADD_NEW_USER = "INSERT INTO users (fullname, username, password, email, phone, country, city, address, afm, role) VALUES (?, ?, MD5(?), ?, ?, ?, ?, ?, ?, ?)";
 
     private ConnectionFactory factory;
@@ -15,7 +16,9 @@ public class UserDAO {
         factory = ConnectionFactory.getInstance(pool);
     }
 
-    public boolean insertUser(User userInfo){
+    public Integer insertUser(User userInfo){
+
+        int ret = -1;
 
         Object[] values = { userInfo.getFullname(), userInfo.getUsername(), userInfo.getPassword(), userInfo.getEmail()
                 , userInfo.getPhone(), userInfo.getCountry(), userInfo.getCity(), userInfo.getAddress(), userInfo.getAfm(),
@@ -27,13 +30,24 @@ public class UserDAO {
         {
             int affectedRows = statement.executeUpdate();
             System.out.println("Affected Rows: " + affectedRows);
+            ret = affectedRows;
+
+            if (ret == 0) {
+                System.err.println("Creating user failed, no rows affected.");
+                return ret;
+            }
+
+        } catch (SQLIntegrityConstraintViolationException e1){
+            return  USERNAME_EXISTS_ERROR;
 
         }
-        catch (SQLException e) {
+         catch (SQLException e) {
             System.err.println(e.getMessage());
+            return ret;
         }
 
-        return true;
+        ret = 1;
+        return ret;
     }
 
 }
--- src/servlets/Register.java
+++ src/servlets/Register.java
@@ -2,6 +2,7 @@ package servlets;
 
 import dao.UserDAO;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,8 +16,10 @@ public class Register extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // Set request encoding to UTF-8
         request.setCharacterEncoding("UTF-8");
 
+        // Get register form fields
         String fullname = request.getParameter("fullname");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -28,20 +31,32 @@ public class Register extends HttpServlet {
         String afm = request.getParameter("afm");
         String role = request.getParameter("UserRole");
 
-        System.out.println("COUNYTY: " + country);
-
         // Create UserDTO
         User userInfo = new User(fullname, username, password, email, phone, country, city, address, afm, Integer.parseInt(role));
 
         // Create UserDAO and Add new user to database
         UserDAO dao = new UserDAO(true);
-        dao.insertUser(userInfo);
+        Integer register_result = dao.insertUser(userInfo);
 
+        // Set response encoding to UTF-8
         response.setContentType("text/html; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
-        
-        response.getWriter().print("Success! Name: " + fullname +
+
+        if (register_result == -2){     // Duplicate Username check
+            response.getWriter().print("UserName Already Exists");
+            response.getWriter().println("<script type=\"text/javascript\">");
+            response.getWriter().println("alert('User or password incorrect');");
+            response.getWriter().println("location='index.jsp';");
+            response.getWriter().println("</script>");
+
+        }
+        else if (register_result == -1) {
+            response.getWriter().print("Error! Please try again.");
+        }
+        else if (register_result == 1){ // No Errors. Register is successful
+            response.getWriter().print("Success! Name: " + fullname +
                 "\nUsername: " + username + "\nPassword: " + password + "\nRole: " + role);
+        }
     }
 
 }
